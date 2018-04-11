286f330767b3e414159355a563438978cc50e83d
Dukhovnyak Aleksandr Gennadyevich <a.dukhovnyak@tinkoff.ru>
2018-04-11 13:39:21 +0300

    Форма логина

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { CustomValidators } from '../services/validators';
+import { ApiService, IUser } from '../services/http';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,9 +10,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  constructor() { }
+  form: FormGroup;
+  inProcess = false;
+
+  constructor(private fb: FormBuilder,
+              private http: ApiService,
+              private router: Router) { }
 
   ngOnInit() {
+    this.form = this.createForm();
+  }
+
+  onSubmit() {
+    const data = this.form.value;
+
+    this.inProcess = true;
+    this.http.loginUser(data).valueChanges()
+      .subscribe((value: IUser[]) => {
+        this.inProcess = false;
+
+        if (value.length) {
+          this.router.navigate(['chat']);
+
+          localStorage.setItem('user', JSON.stringify(value[0]));
+        }
+      });
+  }
+
+  private createForm(): FormGroup {
+    return this.fb.group({
+      name: ['', Validators.required],
+      password: ['', [
+        Validators.required,
+        Validators.minLength(4),
+        CustomValidators.checkPassword
+      ]]
+    });
   }
 
 }
